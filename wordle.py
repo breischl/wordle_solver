fa16d3fe1f8e63fcb3309162f30725a7e0a80537
BrianReischl <breischl@gmail.com>
2022-03-08 09:40:38 -0700

    Cleaned up dictionary file

--- wordle.py
+++ wordle.py
@@ -10,19 +10,29 @@ def load_dictionary() -> list:
     '''Load and return the dictionary as a list[str]
     Returns: list[str]
     '''
-    with open(DICTIONARY_FILE) as word_file:
+    with open(DICTIONARY_FILE, mode="rt") as word_file:
+        valid_words = [w for w in word_file.read().splitlines()]
+    return valid_words
+
+
+def cleanse_dictionary() -> None:
+    '''Read in the dictionary file, remove invalid words, and write the result back'''
+    with open(DICTIONARY_FILE, mode="rt") as word_file:
         valid_words = [w
                        for w in word_file.read().splitlines()
                        if is_valid_word(w)]
-    return valid_words
+
+    save_dictionary(valid_words)
 
 
 def save_dictionary(words: list) -> None:
     '''Save the given list to the dictionary file
     '''
 
-    with open(DICTIONARY_FILE) as word_file:
-        word_file.writelines(words)
+    with open(DICTIONARY_FILE, mode="wt") as word_file:
+        for w in words:
+            word_file.write(w)
+            word_file.write("\n")
 
 
 def is_valid_word(w: str):
