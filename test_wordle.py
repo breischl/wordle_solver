831c2b0500de7d20a7a3959ada25f70a9f4054ec
BrianReischl <breischl@gmail.com>
2022-03-08 09:26:47 -0700

    Fix tests

--- test_wordle.py
+++ test_wordle.py
@@ -29,21 +29,19 @@ class TestWordle(unittest.TestCase):
         self.assertEqual(letter_scores, [
                          wordle.CORRECT, wordle.WRONG, wordle.CORRECT, wordle.MISPLACED, wordle.WRONG])
 
+    def test_check_word_correct(self):
+        (is_correct, letter_scores) = wordle.check_word(
+            solution="tamed", guess="tamed")
+        self.assertTrue(is_correct)
+        self.assertEqual(letter_scores, [
+            wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT])
 
-def test_check_word_correct(self):
-    (is_correct, letter_scores) = wordle.check_word(
-        solution="tamed", guess="tames")
-    self.assertTrue(is_correct)
-    self.assertEqual(letter_scores, [
-        wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT])
+    def test_is_possible_solution(self):
+        self.assertFalse(wordle.is_possible_solution(word="tamed", guess="tummy", letter_scores=[
+            wordle.WRONG, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT]))
 
+        self.assertFalse(wordle.is_possible_solution(word="tamed", guess="tummy", letter_scores=[
+            wordle.MISPLACED, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT]))
 
-def test_is_possible_solution(self):
-    self.assertFalse(wordle.is_possible_solution(word="tamed", guess="tummy", letter_scores=[
-        wordle.WRONG, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT]))
-
-    self.assertFalse(wordle.is_possible_solution(word="tamed", guess="tummy", letter_scores=[
-        wordle.MISPLACED, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.CORRECT]))
-
-    self.assertTrue(wordle.is_possible_solution(word="tamed", guess="tummy", letter_scores=[
-        wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.MISPLACED, wordle.CORRECT]))
+        self.assertTrue(wordle.is_possible_solution(word="tumor", guess="tummr", letter_scores=[
+            wordle.CORRECT, wordle.CORRECT, wordle.CORRECT, wordle.MISPLACED, wordle.CORRECT]))
